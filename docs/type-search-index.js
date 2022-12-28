a4787537bc37cd7002ebee8e06ce09f0961cc6ba
DanielBaumgarten <86086435+DanielBaumgarten@users.noreply.github.com>
2022-12-28 11:06:27 -0300

    <feat>:<atualizações>

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"edu.aulas.javadoc","l":"Calculadora"},{"p":"edu.aulas.javadoc","l":"Comentarios"}];updateSearchResults();
